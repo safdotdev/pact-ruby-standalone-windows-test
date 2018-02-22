890b5f5a611090f50fe871d8d1ee9e867974ef26
Beth Skurrie <beth@bethesque.com>
2018-02-22 13:51:51 +1100

    feat: dynamically lock/yield files based on OS

--- spec/support/monkeypatch.rb
+++ spec/support/monkeypatch.rb
@@ -1,26 +1,38 @@
-
 module Pact
   class ConsumerContractWriter
 
-    def update_pactfile
-      puts "Updating pact file with lock on separate file"
-      logger.info log_message
-      FileUtils.mkdir_p File.dirname(pactfile_path)
-      lockfile_path = pactfile_path + ".lock"
-      Filelock lockfile_path do | file |
-        new_contents = pact_json
-        File.open(pactfile_path, "w") { |file| file << new_contents }
+    def windows?
+      !!RUBY_PLATFORM =~ /mswin|mingw|bccwin|wince|emx/
+    end
+
+    def with_lockfile pactfile_path
+      lockfile_path = windows? ? pactfile_path + ".lock" : pactfile_path
+      begin
+        puts "Locking #{lockfile_path}"
+        Filelock lockfile_path do | locked_file |
+          if windows?
+            File.open(pactfile_path, "a") { |pact_file| puts "yielding #{pactfile_path}"; yield pact_file }
+          else
+            puts "yielding #{locked_file.path}"
+            yield locked_file
+          end
+        end
+      rescue StandardError => e
+        warn_and_stderr("#{e.class} #{e.message}")
+        if windows?
+          File.unlink(lockfile_path) rescue nil
+        end
       end
     end
 
+    def update_pactfile
+      logger.info log_message
+      FileUtils.mkdir_p File.dirname(pactfile_path)
+      with_lockfile(pactfile_path) do | pact_file |
+        new_contents = pact_json
+        pact_file.truncate 0
+        pact_file.write new_contents
+      end
+    end
   end
 end
-
-
-# def Filelock(lockname, options = {}, &block)
-#   lockname = lockname.path if lockname.is_a?(Tempfile)
-#   File.open(lockname, File::RDWR|File::CREAT, 0644) do |file|
-#     Timeout::timeout(options.fetch(:wait, 60*60*24), Filelock::WaitTimeout) { file.flock(File::LOCK_EX) }
-#     Timeout::timeout(options.fetch(:timeout, 60), Filelock::ExecTimeout) { yield file; file.flock(File::LOCK_UN) }
-#   end
-# end
