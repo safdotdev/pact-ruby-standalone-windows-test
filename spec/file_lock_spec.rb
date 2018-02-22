7620526731fd23e570ee40c92e0bbc70e588e128
Beth Skurrie <beth@bethesque.com>
2018-02-22 17:22:46 +1100

    feat: try reading the yielded file

--- spec/file_lock_spec.rb
+++ spec/file_lock_spec.rb
@@ -1,6 +1,19 @@
 require 'filelock'
 
 RSpec.describe "testing locking" do
+
+  it "" do
+    File.open("counter", File::RDWR|File::CREAT, 0644) {|f|
+      f.flock(File::LOCK_EX)
+      value = f.read.to_i + 1
+      f.rewind
+      f.write("#{value}\n")
+      f.flush
+      f.truncate(f.pos)
+    }
+    expect(File.read("counter")).to eq "1\n"
+  end
+
   it "" do
     FileUtils.rm_rf "foo"
     File.open("foo", "w") { |file| file << "content" }
