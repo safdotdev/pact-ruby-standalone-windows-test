fd26d844e0ab59dbd170aa06032814c264775bae
Beth Skurrie <beth@bethesque.com>
2018-02-22 12:05:56 +1100

    feat: try explicitly unlocking the file afterwards

--- spec/support/monkeypatch.rb
+++ spec/support/monkeypatch.rb
@@ -1,6 +1,7 @@
 def Filelock(lockname, options = {}, &block)
-  puts "Opening file without filelock"
+  lockname = lockname.path if lockname.is_a?(Tempfile)
   File.open(lockname, File::RDWR|File::CREAT, 0644) do |file|
-    Timeout::timeout(options.fetch(:timeout, 60), Filelock::ExecTimeout) { yield file }
+    Timeout::timeout(options.fetch(:wait, 60*60*24), Filelock::WaitTimeout) { file.flock(File::LOCK_EX) }
+    Timeout::timeout(options.fetch(:timeout, 60), Filelock::ExecTimeout) { yield file; file.flock(File::LOCK_UN) }
   end
 end
